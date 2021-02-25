3c848ddf389e269f24ff24e0cbfda0e298b14e6b
Petr Šťastný <petr@soptik.tech>
2021-02-25 12:09:48 +0100

    Init

---
+++ dataStructures/SegmentTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by petr on 25/02/2021.
+//
+
+#include "SegmentTree.h"
---
+++ dataStructures/SegmentTree.h
@@ -0,0 +1,14 @@
+//
+// Created by petr on 25/02/2021.
+//
+
+#ifndef ACM_SEGMENTTREE_H
+#define ACM_SEGMENTTREE_H
+
+
+class SegmentTree {
+
+};
+
+
+#endif //ACM_SEGMENTTREE_H
