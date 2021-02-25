b70067b3c84457fdc1a60944a0b03e105bbf3b31
Petr Šťastný <petr@soptik.tech>
2021-02-25 18:05:14 +0100

    Added tree declaration

--- dataStructures/SegmentTree.cpp
+++ dataStructures/SegmentTree.cpp
@@ -1,5 +1,49 @@
-//
-// Created by petr on 25/02/2021.
-//
-
+#include <stdexcept>
 #include "SegmentTree.h"
+
+SegmentTree::SegmentTree(int arrsize, int *arr) {
+    if(arr == nullptr) {
+        this->arr = new int[arrsize];
+    } else {
+        this->arr = arr;
+    }
+    this->n = arrsize;
+
+    buildTree();
+}
+
+void SegmentTree::sanitizeRange(int from, int to) {
+    if ( from < 0 ) throw std::invalid_argument("from cannot be negative");
+    if ( to < 0 ) throw std::invalid_argument("to cannot be negative");
+    if ( from > to ) throw std::invalid_argument("from cannot be higher than to");
+    if ( to >= n ) throw std::invalid_argument("to cannot be that high, out of range of initial array");
+}
+
+int SegmentTree::rangeQuery(int from, int to) {
+    sanitizeRange(from, to);
+    return 0;
+}
+
+int SegmentTree::rangeSet(int from, int to, int value) {
+    sanitizeRange(from, to);
+    return 0;
+}
+
+int SegmentTree::rangeAdd(int from, int to, int delta) {
+    sanitizeRange(from, to);
+    return 0;
+}
+
+SegmentTree::~SegmentTree() {
+    delete [] arr;
+    freeTree(tree);
+}
+
+void SegmentTree::freeTree(SegmentTreeNode * tree) {
+    if(!tree) return;
+
+    freeTree(tree->left);
+    freeTree(tree->right);
+    delete tree;
+}
+
--- dataStructures/SegmentTree.h
+++ dataStructures/SegmentTree.h
@@ -1,13 +1,33 @@
-//
-// Created by petr on 25/02/2021.
-//
+#include <vector>
 
 #ifndef ACM_SEGMENTTREE_H
 #define ACM_SEGMENTTREE_H
 
+struct SegmentTreeNode;
 
 class SegmentTree {
+    int * arr;
+    SegmentTreeNode * tree;
+    int n;
 
+public:
+    SegmentTree(int arrsize, int * arr = nullptr);
+    ~SegmentTree();
+    int rangeQuery(int from, int to);
+    int rangeSet(int from, int to, int value);
+    int rangeAdd(int from, int to, int delta);
+
+private:
+    void buildTree();
+    void freeTree(SegmentTreeNode *pNode);
+    void sanitizeRange(int from, int to);
+};
+
+struct SegmentTreeNode {
+    SegmentTreeNode * left;
+    SegmentTreeNode * right;
+    SegmentTreeNode * parent;
+    int value;
 };
 
 
